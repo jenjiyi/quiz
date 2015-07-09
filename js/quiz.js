be3fe6ceb35fd0c31db081b62d3846fd200ce75b
jenjiyi <jenny.yamada@gmail.com>
2015-07-09 12:27:54 -0700

    added load fadeIn and reset button

---
+++ js/quiz.js
@@ -0,0 +1,17 @@
+$(document).ready (function(){
+//declare global vars
+
+//questions object
+
+//events
+
+//transition animation functions
+	$(window).load(function() {
+		  $(".main").fadeIn(2000); 
+
+		  //$( ".content" ).show( "slide", 1000 );
+		 
+		});
+
+
+});
